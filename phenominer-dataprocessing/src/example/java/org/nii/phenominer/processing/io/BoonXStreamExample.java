c758accad53be25cf0f0569ef7ea68f27cc4a487
lupanh <lupanh@yahoo.com>
2014-06-05 00:22:41 +0700

    new update

--- phenominer-dataprocessing/src/example/java/org/nii/phenominer/processing/io/BoonXStreamExample.java
+++ phenominer-dataprocessing/src/example/java/org/nii/phenominer/processing/io/BoonXStreamExample.java
@@ -1,14 +1,15 @@
 package org.nii.phenominer.processing.io;
 
 import java.io.File;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.boon.IO;
 import org.boon.json.JsonParserAndMapper;
 import org.boon.json.JsonParserFactory;
 import org.boon.json.JsonSerializer;
 import org.boon.json.JsonSerializerFactory;
+import org.nii.phenominer.processing.util.FileHelper;
 
 import com.thoughtworks.xstream.XStream;
 
@@ -20,18 +21,18 @@ public class BoonXStreamExample {
 		List<DocumentExample> docs = new ArrayList<DocumentExample>();
 		docs.add(doc1);
 		docs.add(doc2);
-		
+
 		JsonSerializer serializer = new JsonSerializerFactory().create();
 		XStream xstream = new XStream();
 		xstream.alias("document", DocumentExample.class);
 		xstream.alias("documents", List.class);
-		
+
 		String json = serializer.serialize(doc1).toString();
 		String xml = xstream.toXML(docs);
 		System.out.println(xml);
-		
+
 		File file = File.createTempFile("document", ".json");
-		IO.write(IO.path(file.toString()), json);
+		FileHelper.writeToFile(json, file, Charset.forName("ISO-8859-1"));
 
 		JsonParserAndMapper jsonParserAndMapper = new JsonParserFactory().create();
 		doc1 = jsonParserAndMapper.parse(DocumentExample.class, json);
