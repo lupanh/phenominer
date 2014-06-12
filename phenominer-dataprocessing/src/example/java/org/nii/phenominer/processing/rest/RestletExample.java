368969cb7f695f0d6a400301505f4a76380a69e3
lupanh <lupanh@yahoo.com>
2014-06-12 21:51:26 +0700

    new update

--- phenominer-dataprocessing/src/example/java/org/nii/phenominer/processing/rest/RestletExample.java
+++ phenominer-dataprocessing/src/example/java/org/nii/phenominer/processing/rest/RestletExample.java
@@ -8,27 +8,21 @@ import org.restlet.routing.Router;
 
 public class RestletExample extends Application {
 	public static void main(String[] args) throws Exception {
-		// Create a component
 		Component component = new Component();
 		component.getServers().add(Protocol.HTTP, 8111);
 
-		// Create an application
 		Application application = new RestletExample();
 
-		// Attach the application to the component and start it
 		component.getDefaultHost().attachDefault(application);
 		component.start();
 	}
 
 	@Override
 	public Restlet createInboundRoot() {
-		// Create a router
 		Router router = new Router(getContext());
+		
+		router.attach("/bllip?text={text}", BllipParserService.class);
 
-		// Attach the resources to the router
-		router.attach("/users={user}", UserResource.class);
-
-		// Return the root router
 		return router;
 	}
 
