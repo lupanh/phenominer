8bcc176c27ab89c39b5e38fc5a8ecef69ec681a1
lupanh <lupanh@yahoo.com>
2014-06-12 17:19:15 +0700

    new update

---
+++ phenominer-datacollector/src/example/java/org/nii/phenominer/collector/restful/JerseyClientExample.java
@@ -0,0 +1,18 @@
+package org.nii.phenominer.collector.restful;
+
+import java.io.IOException;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.client.ClientConfig;
+
+
+public class JerseyClientExample {
+
+	public static void main(String[] args) {
+
+	}
+
+}
